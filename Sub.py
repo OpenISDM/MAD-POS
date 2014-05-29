48ae0033bb531e0303fedac0a39fd608f9b9bed1
root <root@Sub.iis.sinica.edu.tw>
2014-05-29 17:51:25 +0800

    fix some code

--- Sub.py
+++ Sub.py
@@ -7,8 +7,6 @@ import requests
 
 app = Flask(__name__)
 
-topic_url = ''
-
 
 @app.route('/callback', methods=['POST'])
 def callbackPost():
@@ -47,14 +45,19 @@ def callbackGet():
 class startServer:
 
     def __init__(self, longlat, postype, isurl):
+        print 'initialize.....'
         self.long_lat = longlat
         self.pos_type = postype
         self.is_url = isurl
         self.hub_url = ''
         self.topic_url = ''
+        
 
     def discovery(self):
+        print 'discovery.....'
         payload = {'longlat': self.long_lat, 'postype': self.pos_type}
+        print payload
+        print self.is_url
         r = requests.get(self.is_url, params=payload)
         print r.headers
         print 'Hub = %s' % r.links["hub"]["url"]
@@ -63,17 +66,21 @@ class startServer:
         self.topic_url = r.links["self"]["url"]
 
     def subscribe(self):
+        print 'subscirbe.....'
         headers = {'content-type': 'application/x-www-form-urlencoded'}
         payload = {'hub.mode': 'subscribe', 'hub.topic': self.topic_url,
-                   'hub.callback': 'http://localhost:8888/callback'}
+                   'hub.callback': 'http://140.109.22.153/callback'}
         url = self.hub_url
+        print payload
+        print url
         r = requests.post(url, data=payload, headers=headers)
         print 'sub.text %s' % r.text
         print 'sub.headers %s' % r.headers
 
 if __name__ == '__main__':
-    # start = startServer('234023423,234234234', 'fixed', 'http://localhost/hub')
-    # start.discovery()
-    # start.subscribe()
+    print 'Start Server.....'
+    start = startServer('234023423,234234234', 'fixed', 'http://140.109.22.227/hub')
+    start.discovery()
+    start.subscribe()
     app.debug = True
-    app.run(host='0.0.0.0', port=int("888"))
+    app.run(host='140.109.22.153', port=int("80"))
