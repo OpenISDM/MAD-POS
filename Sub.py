92c58742f07cf53bd7311421f3bac4d4bb1cd7a7
Johnson <johnsonsu@iis.sinica.edu.tw>
2014-06-03 12:13:28 +0800

    ko

--- Sub.py
+++ Sub.py
@@ -2,23 +2,27 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, request, after_this_request, make_response, \
-    render_template
+    render_template, url_for, redirect, abort
 import requests
 import sys
 
 app = Flask(__name__)
+
+topic_url = 'http://140.109.22.181:8080/topic'
+
 
 @app.route('/', methods=['GET'])
 def root():
     print >> sys.stderr, "Start Server.....#1"
     return 'Server Check OK!!!!'
 
+
 @app.route('/callback', methods=['POST'])
 def callbackPost():
     f = request.files['file']
-    print request.headers
+    print >> sys.stderr, request.headers
     convert_filename_str = str(f)
-    filename = convert_filename_str.rsplit("'",3)[1]
+    filename = convert_filename_str.rsplit("'", 3)[1]
     f.save('./Cache/' + filename)
     return 'POST OK'
 
@@ -30,63 +34,71 @@ def callbackGet():
         resp = make_response(render_template('Accepted.html'), 202)
         return resp
     elif query_string.get('hub.mode') == 'subscribe' or query_string.get('hub.mode') == 'unsubscribe':
+        app.logger.debug(topic_url)
+        app.logger.debug(query_string.get('hub.topic'))
         if topic_url == query_string.get('hub.topic'):
             if 'hub.challenge' in query_string:
-                print query_string.get('hub.challenge')
+                app.logger.debug(query_string.get('hub.challenge'))
                 return query_string.get('hub.challenge')
             else:
-                print 'No value of hub.challenge, topic url is right'
+                print >> sys.stderr, 'No value of hub.challenge, topic url is right'
                 return 'No value of hub.challenge, topic url is right'
         else:
             resp = make_response(render_template('Unknown.html'), 406)
-            print '406'
             return resp
     else:
         resp = make_response(render_template('Unknown.html'), 406)
-        print '406'
         return resp
 
 
 class startServer:
 
     def __init__(self, longlat, postype, isurl):
-        print 'initialize.....'
+        print >> sys.stderr, '...initialize...'
         self.long_lat = longlat
         self.pos_type = postype
         self.is_url = isurl
         self.hub_url = ''
         self.topic_url = ''
-        
 
     def discovery(self):
-        print 'discovery.....'
-        payload = {'longlat': self.long_lat, 'postype': self.pos_type}
-        print payload
-        print self.is_url
+        print >> sys.stderr, '...discovery...'
+
+        payload = {
+            'longlat': self.long_lat,
+            'postype': self.pos_type}
+
         r = requests.get(self.is_url, params=payload)
-        print r.headers
-        print 'Hub = %s' % r.links["hub"]["url"]
-        print 'Topic = %s' % r.links["self"]["url"]
-        self.hub_url = r.links["hub"]["url"]
+
+        self.hub_url   = r.links["hub"]["url"]
         self.topic_url = r.links["self"]["url"]
+        print >> sys.stderr,  'Hub = %s' % self.hub_url
+        print >> sys.stderr,  'Topic = %s' % self.topic_url
 
     def subscribe(self):
-        print 'subscirbe.....'
-        print >> sys.stderr, "subscirbe....."
-        headers = {'content-type': 'application/x-www-form-urlencoded'}
-        payload = {'hub.mode': 'subscribe', 'hub.topic': self.topic_url,
-                   'hub.callback': 'http://140.109.22.153/callback'}
-        url = self.hub_url
-        print payload
-        print url
-        r = requests.post(url, data=payload, headers=headers)
-        print 'sub.text %s' % r.text
-        print 'sub.headers %s' % r.headers
+        print >> sys.stderr, "...subscribe..."
+
+        headers = {
+            'content-type': 'application/x-www-form-urlencoded'}
+        payload = {
+            'hub.mode': 'subscribe',
+            'hub.topic': self.topic_url,
+            'hub.callback': 'http://140.109.22.181:8888/callback'}
+
+        print >> sys.stderr, "payload..."
+        print >> sys.stderr,  payload
+
+        r = requests.post(self.hub_url, data=payload, headers=headers)
+
+        print >> sys.stderr, 'sub.status_code %s' % r.status_code
+        print >> sys.stderr, 'sub.text        %s' % r.text
+        print >> sys.stderr, 'sub.headers     %s' % r.headers
+
 
 if __name__ == '__main__':
-    print >> sys.stderr, "Start Server.....#2"
-    start = startServer('234023423,234234234', 'fixed', 'http://140.109.22.227/hub')
+    start = startServer(
+        '234023423,234234234', 'fixed', 'http://140.109.22.181:8080/hub')
     start.discovery()
     start.subscribe()
     app.debug = True
-    #app.run(host='140.109.22.153', port=int("80"))
+    app.run(host='140.109.22.181', port=int("8888"), threaded=True)
